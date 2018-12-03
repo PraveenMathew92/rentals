497186f680177430f5cf8dfa2828494c73ada5e4
Praveen Mathew <mpraveen@thoughtworks.com>
2018-12-03 22:59:46 +0530

    Unset JMX reporting for cluster

--- src/main/kotlin/com/example/rentals/ReactiveCassandraConfiguration.kt
+++ src/main/kotlin/com/example/rentals/ReactiveCassandraConfiguration.kt
@@ -2,6 +2,7 @@ package com.example.rentals
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean
 import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories
 
 
@@ -9,4 +10,9 @@ import org.springframework.data.cassandra.repository.config.EnableReactiveCassan
 @EnableReactiveCassandraRepositories
 class ReactiveCassandraConfiguration: AbstractReactiveCassandraConfiguration() {
     override fun getKeyspaceName(): String = "rentals"
+    override fun cluster(): CassandraClusterFactoryBean {
+        val cluster = super.cluster()
+        cluster.setJmxReportingEnabled(false)
+        return cluster
+    }
 }
