e04e825719c45e932ef24ce87834b9656e6b3fa7
Praveen Mathew <mpraveen@thoughtworks.com>
2018-12-04 01:36:03 +0530

    Create keyspace on cluster initialization

--- src/main/kotlin/com/example/rentals/ReactiveCassandraConfiguration.kt
+++ src/main/kotlin/com/example/rentals/ReactiveCassandraConfiguration.kt
@@ -3,16 +3,27 @@ package com.example.rentals
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification
 import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories
 
 
 @Configuration
 @EnableReactiveCassandraRepositories
 class ReactiveCassandraConfiguration: AbstractReactiveCassandraConfiguration() {
-    override fun getKeyspaceName(): String = "rentals"
+    private val KEYSPACE_NAME = "rentals"
+
+    override fun getKeyspaceName(): String = KEYSPACE_NAME
+
     override fun cluster(): CassandraClusterFactoryBean {
         val cluster = super.cluster()
         cluster.setJmxReportingEnabled(false)
         return cluster
+    }
+
+    override fun getKeyspaceCreations(): MutableList<CreateKeyspaceSpecification> {
+        return mutableListOf(CreateKeyspaceSpecification
+                .createKeyspace(KEYSPACE_NAME)
+                .withSimpleReplication(1)
+                .ifNotExists())
     }
 }
