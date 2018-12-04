4a8c407d158c90fc6c28c4d85df78c0281c2e5b7
Praveen Mathew <praveen.mathew92@outlook.com>
2018-12-04 23:16:20 +0530

    Controller returns appropriate http status on saving the asset

--- src/main/kotlin/com/example/rentals/Controller/AssetController.kt
+++ src/main/kotlin/com/example/rentals/Controller/AssetController.kt
@@ -1,16 +1,23 @@
-package com.example.rentals.Controller
+package com.example.rentals.controller
 
 import com.example.rentals.domain.Asset
-import com.example.rentals.repository.AssetRepository
+import com.example.rentals.service.AssetService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/asset")
-class AssetController(val repository: AssetRepository){
+class AssetController(private val assetService: AssetService) {
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody asset: Asset) {
-        repository.save(asset)
+    fun create(@RequestBody asset: Asset): Mono<ResponseEntity<Asset>> {
+        return assetService
+                .create(asset)
+                .map { it -> if(it) ResponseEntity(asset, HttpStatus.CREATED)
+                    else ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY)}
     }
 }
--- src/test/kotlin/com/example/rentals/Controller/AssetControllerTest.kt
+++ src/test/kotlin/com/example/rentals/Controller/AssetControllerTest.kt
@@ -1,25 +1,58 @@
-package com.example.rentals.Controller
+package com.example.rentals.controller
 
+import com.example.rentals.service.AssetService
 import com.example.rentals.domain.Asset
-import com.example.rentals.repository.AssetRepository
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import reactor.core.publisher.Mono
 import java.util.*
 
 internal class AssetControllerTest{
-    val assetRepository = Mockito.mock(AssetRepository::class.java)
-
-    private val assetController = AssetController(assetRepository)
+    val assetService = Mockito.mock(AssetService::class.java)
 
     @Test
     fun `should add the asset passed in the request to database`(){
+        val assetController = AssetController(assetService)
+
         val asset = Asset(UUID.fromString("65cf3c7c-f449-4cd4-85e1-bc61dd2db64e"),
                 "Some Asset",
                 "Category")
+        whenever(assetService.create(asset)).thenReturn(Mono.just(true))
+
         assetController.create(asset)
 
-        verify(assetRepository, times(1)).save(asset)
+        verify(assetService, times(1)).create(asset)
+    }
+
+    @Test
+    fun `should return the status 201 when the save is successful`(){
+        val assetController = AssetController(assetService)
+
+        val asset = Asset(UUID.fromString("65cf3c7c-f449-4cd4-85e1-bc61dd2db64e"),
+                "Some Asset",
+                "Category")
+
+        whenever(assetService.create(asset)).thenReturn(Mono.just(true))
+
+        assertEquals(ResponseEntity(asset, HttpStatus.CREATED), assetController.create(asset).block())
+    }
+
+    @Test
+    fun `should return the status 422 when the save is unsuccessful`(){
+        val assetController = AssetController(assetService)
+
+        val asset = Asset(UUID.fromString("65cf3c7c-f449-4cd4-85e1-bc61dd2db64e"),
+                "Some Asset",
+                "Category")
+
+        whenever(assetService.create(asset)).thenReturn(Mono.just(false))
+
+        assertEquals(ResponseEntity<Asset>(HttpStatus.UNPROCESSABLE_ENTITY), assetController.create(asset).block())
     }
 }
