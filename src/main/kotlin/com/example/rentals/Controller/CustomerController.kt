726ea2de6ef09fd813c26b045caa653e11261107
Praveen Mathew <praveen.mathew92@outlook.com>
2018-12-22 00:06:41 +0530

    Add Rest Controller in Customer Service

--- src/main/kotlin/com/example/rentals/Controller/CustomerController.kt
+++ src/main/kotlin/com/example/rentals/Controller/CustomerController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -15,7 +16,8 @@ import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
 import javax.validation.Valid
 
-@RestController("/customer")
+@RestController
+@RequestMapping("/customer")
 class CustomerController(val customerService: CustomerService) {
     @PostMapping
     fun create(@Valid @RequestBody customer: Customer): Mono<ResponseEntity<Customer>> {
@@ -43,7 +45,7 @@ class CustomerController(val customerService: CustomerService) {
     }
 
     @PatchMapping("/{email}")
-    fun patch(@PathVariable email: String, patch: String): Mono<ResponseEntity<Customer>> {
+    fun patch(@PathVariable email: String, @RequestBody patch: String): Mono<ResponseEntity<Customer>> {
         return customerService.patch(email, patch).flatMap { when (it) {
             true -> ResponseEntity<Customer>(HttpStatus.NO_CONTENT).toMono()
             else -> ResponseEntity<Customer>(HttpStatus.NOT_FOUND).toMono()
